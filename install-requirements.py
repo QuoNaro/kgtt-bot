c05fb38d802f4a9d967762dd64c1f4ab38fb0ec2
quonaro <quonaro@mail.ru>
2023-09-10 16:49:51 +0700

    update requirements

--- install-requirements.py
+++ install-requirements.py
@@ -4,6 +4,7 @@ import os
 # Установка глобальных зависимостей проекта
 # TODO : Добавить обновление суб-модулей git
 
+os.system('git submodule init && git submodule update')
 os.system('pip install --upgrade pip')
 os.system('pip install -r requirements.txt')
 
@@ -15,4 +16,4 @@ submodule_folder_names = os.listdir(f'{current_dir}/git_submodules/')
 for submodule_folder_name in submodule_folder_names:
     # Установка модулей и их локальных зависимостей
     os.system(f'pip install {current_dir}/git_submodules/{submodule_folder_name}')
-
+    
