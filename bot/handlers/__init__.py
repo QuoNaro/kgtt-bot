a97f7b07d32e2a67efc7f1bed0a9a32570539294
quonaro <quonaro@mail.ru>
2023-09-17 15:57:37 +0700

    add init

---
+++ bot/handlers/__init__.py
@@ -0,0 +1,4 @@
+from bot.handlers.common import * # noqa: F403
+from bot.handlers.marks import * # noqa: F403
+from bot.handlers.parameters import * # noqa: F403
+from bot.handlers.table import * # noqa: F403
